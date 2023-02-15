82a779a8dc6d9055df35744842fe63ec5268a3ca
Zhao KK <kikakkz@hotmail.com>
2023-02-15 10:58:40 +0000

    Support fill templates

--- pkg/template/sms/fill.go
+++ pkg/template/sms/fill.go
@@ -6,7 +6,16 @@ import (
 	basetypes "github.com/NpoolPlatform/message/npool/basetypes/v1"
 	npool "github.com/NpoolPlatform/message/npool/notif/mw/v1/template"
 
+	chanmgrpb "github.com/NpoolPlatform/message/npool/notif/mgr/v1/channel"
 	notifmgrpb "github.com/NpoolPlatform/message/npool/notif/mgr/v1/notif"
+
+	smstmplmgrpb "github.com/NpoolPlatform/message/npool/notif/mgr/v1/template/sms"
+	smstmplmgrcli "github.com/NpoolPlatform/notif-manager/pkg/client/template/sms"
+
+	cruder "github.com/NpoolPlatform/libent-cruder/pkg/cruder"
+	commonpb "github.com/NpoolPlatform/message/npool"
+
+	tmplreplace "github.com/NpoolPlatform/notif-middleware/pkg/template/replace"
 )
 
 func FillTemplate(
@@ -17,5 +26,43 @@ func FillTemplate(
 ) (
 	[]*notifmgrpb.NotifReq, error,
 ) {
-	return nil, nil
+	const maxTemplates = int32(100)
+
+	tmpls, _, err := smstmplmgrcli.GetSMSTemplates(ctx, &smstmplmgrpb.Conds{
+		AppID: &commonpb.StringVal{
+			Op:    cruder.EQ,
+			Value: appID,
+		},
+		UsedFor: &commonpb.Int32Val{
+			Op:    cruder.EQ,
+			Value: int32(usedFor),
+		},
+	}, 0, maxTemplates)
+	if err != nil {
+		return nil, err
+	}
+	if len(tmpls) == 0 {
+		return nil, nil
+	}
+
+	reqs := []*notifmgrpb.NotifReq{}
+	for _, tmpl := range tmpls {
+		title := tmplreplace.ReplaceAll(tmpl.Subject, vars)
+		content := tmplreplace.ReplaceAll(tmpl.Message, vars)
+		useTemplate := true
+		channel1 := chanmgrpb.NotifChannel_ChannelSMS
+
+		reqs = append(reqs, &notifmgrpb.NotifReq{
+			AppID:       &appID,
+			UserID:      &userID,
+			LangID:      &tmpl.LangID,
+			EventType:   &usedFor,
+			UseTemplate: &useTemplate,
+			Title:       &title,
+			Content:     &content,
+			Channel:     &channel1,
+		})
+	}
+
+	return reqs, nil
 }

c83cd5176b05d8e990a04476df1a4a16b525991c
Zhao KK <kikakkz@hotmail.com>
2023-05-24 05:17:26 +0000

    Support format amount

--- pkg/template/replace/replace.go
+++ pkg/template/replace/replace.go
@@ -21,6 +21,25 @@ const (
 	NotifTemplateVarCode     = "{{ CODE }}"
 )
 
+//nolint
+func formatAmount(amount string) string {
+	index := strings.Index(amount, ".")
+	if index < 0 {
+		index = len(amount)
+	}
+	_amount := []byte(amount[index:])
+	count := 0
+	for i := index; i > 0; i-- {
+		_amount = append([]byte(amount[i-1:i]), _amount[0:]...)
+		count++
+		if count == 3 && i > 1 {
+			_amount = append([]byte(","), _amount[0:]...)
+			count = 0
+		}
+	}
+	return string(_amount)
+}
+
 func ReplaceAll(pattern string, vars *npool.TemplateVars) string {
 	if vars == nil {
 		return pattern
@@ -34,7 +53,7 @@ func ReplaceAll(pattern string, vars *npool.TemplateVars) string {
 	}
 	if vars.Amount != nil {
 		p := message.NewPrinter(language.English)
-		pattern = strings.ReplaceAll(pattern, NotifTemplateVarAmount, p.Sprintf("%d", *vars.Amount))
+		pattern = strings.ReplaceAll(pattern, NotifTemplateVarAmount, p.Sprintf("%s", formatAmount(*vars.Amount)))
 	}
 	if vars.CoinUnit != nil {
 		pattern = strings.ReplaceAll(pattern, NotifTemplateVarCoinUnit, *vars.CoinUnit)

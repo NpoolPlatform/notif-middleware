414d22199bb62b008cfa503266cbb890aeb5982b
daqiangli <dqli161@163.com>
2023-06-12 10:21:42 +0800

    add amt query

---
+++ pkg/mw/announcement/query.go
@@ -0,0 +1,143 @@
+package announcement
+
+import (
+	"context"
+	"fmt"
+
+	npool "github.com/NpoolPlatform/message/npool/notif/mw/v1/announcement"
+	crud "github.com/NpoolPlatform/notif-middleware/pkg/crud/announcement"
+	"github.com/NpoolPlatform/notif-middleware/pkg/db"
+	"github.com/NpoolPlatform/notif-middleware/pkg/db/ent"
+	entamt "github.com/NpoolPlatform/notif-middleware/pkg/db/ent/announcement"
+)
+
+type queryHandler struct {
+	*Handler
+	stm   *ent.AnnouncementSelect
+	infos []*npool.Announcement
+	total uint32
+}
+
+func (h *queryHandler) selectAnnouncement(stm *ent.AnnouncementQuery) {
+	h.stm = stm.Select(
+		entamt.FieldID,
+		entamt.FieldAppID,
+		entamt.FieldLangID,
+		entamt.FieldTitle,
+		entamt.FieldContent,
+		entamt.FieldChannel,
+		entamt.FieldType,
+		entamt.FieldEndAt,
+		entamt.FieldCreatedAt,
+		entamt.FieldUpdatedAt,
+	)
+}
+
+func (h *queryHandler) queryAnnouncement(cli *ent.Client) error {
+	if h.ID == nil {
+		return fmt.Errorf("invalid announcement id")
+	}
+	h.selectAnnouncement(
+		cli.Announcement.
+			Query().
+			Where(
+				entamt.ID(*h.ID),
+				entamt.DeletedAt(0),
+			),
+	)
+	return nil
+}
+
+func (h *queryHandler) queryAnnouncementsByConds(ctx context.Context, cli *ent.Client) (err error) {
+	stm, err := crud.SetQueryConds(cli.Announcement.Query(), h.Conds)
+	if err != nil {
+		return err
+	}
+
+	total, err := stm.Count(ctx)
+	if err != nil {
+		return err
+	}
+
+	h.total = uint32(total)
+
+	h.selectAnnouncement(stm)
+	return nil
+}
+
+func (h *queryHandler) scan(ctx context.Context) error {
+	return h.stm.Scan(ctx, &h.infos)
+}
+
+func (h *Handler) GetAnnouncements(ctx context.Context) ([]*npool.Announcement, uint32, error) {
+	handler := &queryHandler{
+		Handler: h,
+	}
+
+	err := db.WithClient(ctx, func(_ctx context.Context, cli *ent.Client) error {
+		if err := handler.queryAnnouncementsByConds(_ctx, cli); err != nil {
+			return err
+		}
+
+		handler.
+			stm.
+			Offset(int(h.Offset)).
+			Limit(int(h.Limit))
+		if err := handler.scan(_ctx); err != nil {
+			return err
+		}
+		return nil
+	})
+	if err != nil {
+		return nil, 0, err
+	}
+
+	return handler.infos, handler.total, nil
+}
+
+func (h *Handler) GetAnnouncement(ctx context.Context) (info *npool.Announcement, err error) {
+	handler := &queryHandler{
+		Handler: h,
+	}
+
+	err = db.WithClient(ctx, func(_ctx context.Context, cli *ent.Client) error {
+		if err := handler.queryAnnouncement(cli); err != nil {
+			return err
+		}
+		if err := handler.scan(_ctx); err != nil {
+			return err
+		}
+		return nil
+	})
+	if err != nil {
+		return
+	}
+
+	if len(handler.infos) == 0 {
+		return nil, nil
+	}
+
+	return handler.infos[0], nil
+}
+
+func (h *Handler) GetAnnouncementConds(ctx context.Context) ([]*npool.Announcement, uint32, error) {
+	handler := &queryHandler{
+		Handler: h,
+	}
+
+	err := db.WithClient(ctx, func(_ctx context.Context, cli *ent.Client) error {
+		if err := handler.queryAnnouncementsByConds(_ctx, cli); err != nil {
+			return err
+		}
+
+		if err := handler.scan(_ctx); err != nil {
+			return err
+		}
+		return nil
+	})
+	if err != nil {
+		return nil, 0, err
+	}
+
+	return handler.infos, handler.total, nil
+}

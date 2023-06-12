25cf37a3496a9503c959b51a1b36b72d946db284
daqiangli <dqli161@163.com>
2023-06-12 14:43:40 +0800

    add amt user query

---
+++ pkg/mw/announcement/user/query.go
@@ -0,0 +1,143 @@
+package user
+
+import (
+	"context"
+	"fmt"
+
+	"entgo.io/ent/dialect/sql"
+	npool "github.com/NpoolPlatform/message/npool/notif/mw/v1/announcement/user"
+	crud "github.com/NpoolPlatform/notif-middleware/pkg/crud/announcement/user"
+	"github.com/NpoolPlatform/notif-middleware/pkg/db"
+	"github.com/NpoolPlatform/notif-middleware/pkg/db/ent"
+	entamt "github.com/NpoolPlatform/notif-middleware/pkg/db/ent/announcement"
+	entuseramt "github.com/NpoolPlatform/notif-middleware/pkg/db/ent/userannouncement"
+)
+
+type queryHandler struct {
+	*Handler
+	stm   *ent.UserAnnouncementSelect
+	infos []*npool.UserAnnouncement
+	total uint32
+}
+
+func (h *queryHandler) selectUserAnnouncement(stm *ent.UserAnnouncementQuery) {
+	h.stm = stm.Select(
+		entamt.FieldID,
+		entamt.FieldAppID,
+		entamt.FieldCreatedAt,
+		entamt.FieldUpdatedAt,
+	)
+}
+
+func (h *queryHandler) queryJoinAnnouncement(s *sql.Selector) {
+	t := sql.Table(entamt.Table)
+	s.LeftJoin(t).
+		On(
+			s.C(entuseramt.FieldAnnouncementID),
+			t.C(entamt.FieldID),
+		).
+		AppendSelect(
+			t.C(entamt.FieldLangID),
+			t.C(entamt.FieldTitle),
+			t.C(entamt.FieldContent),
+			t.C(entamt.FieldType),
+			t.C(entamt.FieldChannel),
+			t.C(entamt.FieldEndAt),
+		)
+}
+
+func (h *queryHandler) queryJoin() {
+	h.stm.Modify(func(s *sql.Selector) {
+		h.queryJoinAnnouncement(s)
+	})
+}
+
+func (h *queryHandler) queryUserAnnouncement(cli *ent.Client) error {
+	if h.ID == nil {
+		return fmt.Errorf("invalid user announcement id")
+	}
+	h.selectUserAnnouncement(
+		cli.UserAnnouncement.
+			Query().
+			Where(
+				entuseramt.ID(*h.ID),
+				entuseramt.DeletedAt(0),
+			),
+	)
+	return nil
+}
+
+func (h *queryHandler) queryUserAnnouncementsByConds(ctx context.Context, cli *ent.Client) (err error) {
+	stm, err := crud.SetQueryConds(cli.UserAnnouncement.Query(), h.Conds)
+	if err != nil {
+		return err
+	}
+
+	total, err := stm.Count(ctx)
+	if err != nil {
+		return err
+	}
+
+	h.total = uint32(total)
+
+	h.selectUserAnnouncement(stm)
+	return nil
+}
+
+func (h *queryHandler) scan(ctx context.Context) error {
+	return h.stm.Scan(ctx, &h.infos)
+}
+
+func (h *Handler) GetUserAnnouncements(ctx context.Context) ([]*npool.UserAnnouncement, uint32, error) {
+	handler := &queryHandler{
+		Handler: h,
+	}
+
+	err := db.WithClient(ctx, func(_ctx context.Context, cli *ent.Client) error {
+		if err := handler.queryUserAnnouncementsByConds(_ctx, cli); err != nil {
+			return err
+		}
+
+		handler.queryJoin()
+		handler.
+			stm.
+			Offset(int(h.Offset)).
+			Limit(int(h.Limit))
+		if err := handler.scan(_ctx); err != nil {
+			return err
+		}
+		return nil
+	})
+	if err != nil {
+		return nil, 0, err
+	}
+
+	return handler.infos, handler.total, nil
+}
+
+func (h *Handler) GetUserAnnouncement(ctx context.Context) (info *npool.UserAnnouncement, err error) {
+	handler := &queryHandler{
+		Handler: h,
+	}
+
+	err = db.WithClient(ctx, func(_ctx context.Context, cli *ent.Client) error {
+		if err := handler.queryUserAnnouncement(cli); err != nil {
+			return err
+		}
+
+		handler.queryJoin()
+		if err := handler.scan(_ctx); err != nil {
+			return err
+		}
+		return nil
+	})
+	if err != nil {
+		return
+	}
+
+	if len(handler.infos) == 0 {
+		return nil, nil
+	}
+
+	return handler.infos[0], nil
+}

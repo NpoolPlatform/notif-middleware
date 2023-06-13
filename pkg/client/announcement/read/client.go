6e6747e0d337a7a4f7f067950dbab70a9f7226b3
daqiangli <dqli161@163.com>
2023-06-13 16:05:50 +0800

    update read ut

--- pkg/client/announcement/read/client.go
+++ pkg/client/announcement/read/client.go
@@ -1,74 +1,99 @@
-package readstate
+package read
 
 import (
 	"context"
 	"fmt"
 	"time"
 
-	mgrpb "github.com/NpoolPlatform/message/npool/notif/mgr/v1/announcement/readstate"
-
 	grpc2 "github.com/NpoolPlatform/go-service-framework/pkg/grpc"
+	"github.com/NpoolPlatform/notif-middleware/pkg/servicename"
 
 	"github.com/NpoolPlatform/libent-cruder/pkg/cruder"
 
 	npool "github.com/NpoolPlatform/message/npool/notif/mw/v1/announcement/read"
-
-	constant "github.com/NpoolPlatform/notif-middleware/pkg/message/const"
 )
 
-var timeout = 10 * time.Second
-
-type handler func(context.Context, npool.MiddlewareClient) (cruder.Any, error)
-
-func withCRUD(ctx context.Context, handler handler) (cruder.Any, error) {
-	_ctx, cancel := context.WithTimeout(ctx, timeout)
+func do(ctx context.Context, fn func(_ctx context.Context, cli npool.MiddlewareClient) (cruder.Any, error)) (cruder.Any, error) {
+	_ctx, cancel := context.WithTimeout(ctx, 10*time.Second) //nolint
 	defer cancel()
 
-	conn, err := grpc2.GetGRPCConn(constant.ServiceName, grpc2.GRPCTAG)
+	conn, err := grpc2.GetGRPCConn(servicename.ServiceDomain, grpc2.GRPCTAG)
 	if err != nil {
-		return nil, fmt.Errorf("fail get readstate connection: %v", err)
+		return nil, err
 	}
-
 	defer conn.Close()
 
 	cli := npool.NewMiddlewareClient(conn)
 
-	return handler(_ctx, cli)
+	return fn(_ctx, cli)
 }
 
-func GetReadState(ctx context.Context, announcementID, userID string) (*npool.ReadState, error) {
-	info, err := withCRUD(ctx, func(_ctx context.Context, cli npool.MiddlewareClient) (cruder.Any, error) {
-		resp, err := cli.GetReadState(ctx, &npool.GetReadStateRequest{
-			AnnouncementID: announcementID,
-			UserID:         userID,
+func CreateReadAnnouncement(ctx context.Context, in *npool.ReadAnnouncementReq) (*npool.ReadAnnouncement, error) {
+	info, err := do(ctx, func(_ctx context.Context, cli npool.MiddlewareClient) (cruder.Any, error) {
+		resp, err := cli.CreateReadAnnouncement(ctx, &npool.CreateReadAnnouncementRequest{
+			Info: in,
 		})
 		if err != nil {
-			return nil, fmt.Errorf("fail get readstate: %v", err)
+			return nil, err
 		}
-		return resp.GetInfo(), nil
+		return resp.Info, nil
 	})
 	if err != nil {
-		return nil, fmt.Errorf("fail get readstate: %v", err)
+		return nil, err
 	}
-	return info.(*npool.ReadState), nil
+	return info.(*npool.ReadAnnouncement), nil
 }
 
-func GetReadStates(ctx context.Context, conds *mgrpb.Conds, offset, limit int32) ([]*npool.ReadState, uint32, error) {
+func DeleteReadAnnouncement(ctx context.Context, id string) (*npool.ReadAnnouncement, error) {
+	info, err := do(ctx, func(_ctx context.Context, cli npool.MiddlewareClient) (cruder.Any, error) {
+		resp, err := cli.DeleteReadAnnouncement(ctx, &npool.DeleteReadAnnouncementRequest{
+			Info: &npool.ReadAnnouncementReq{
+				ID: &id,
+			},
+		})
+		if err != nil {
+			return nil, err
+		}
+		return resp.Info, nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	return info.(*npool.ReadAnnouncement), nil
+}
+
+func GetReadAnnouncements(ctx context.Context, conds *npool.Conds, offset, limit int32) ([]*npool.ReadAnnouncement, uint32, error) {
 	var total uint32
-	infos, err := withCRUD(ctx, func(_ctx context.Context, cli npool.MiddlewareClient) (cruder.Any, error) {
-		resp, err := cli.GetReadStates(ctx, &npool.GetReadStatesRequest{
+	infos, err := do(ctx, func(_ctx context.Context, cli npool.MiddlewareClient) (cruder.Any, error) {
+		resp, err := cli.GetReadAnnouncements(ctx, &npool.GetReadAnnouncementsRequest{
 			Conds:  conds,
 			Limit:  limit,
 			Offset: offset,
 		})
 		if err != nil {
-			return nil, fmt.Errorf("fail get readstates: %v", err)
+			return nil, fmt.Errorf("fail get read announcements: %v", err)
 		}
 		total = resp.GetTotal()
 		return resp.GetInfos(), nil
 	})
 	if err != nil {
-		return nil, 0, fmt.Errorf("fail get readstates: %v", err)
+		return nil, 0, fmt.Errorf("fail get read announcements: %v", err)
 	}
-	return infos.([]*npool.ReadState), total, nil
+	return infos.([]*npool.ReadAnnouncement), total, nil
+}
+
+func GetReadAnnouncement(ctx context.Context, id string) (*npool.ReadAnnouncement, error) {
+	info, err := do(ctx, func(_ctx context.Context, cli npool.MiddlewareClient) (cruder.Any, error) {
+		resp, err := cli.GetReadAnnouncement(ctx, &npool.GetReadAnnouncementRequest{
+			ID: id,
+		})
+		if err != nil {
+			return nil, err
+		}
+		return resp.Info, nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	return info.(*npool.ReadAnnouncement), nil
 }

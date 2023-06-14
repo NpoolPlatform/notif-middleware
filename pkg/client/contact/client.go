92e4497970ce5a9f795db95b3d17c2ac3a92a07d
daqiangli <dqli161@163.com>
2023-06-14 14:46:11 +0800

    add contact client

--- pkg/client/contact/client.go
+++ pkg/client/contact/client.go
@@ -6,35 +6,29 @@ import (
 	"time"
 
 	grpc2 "github.com/NpoolPlatform/go-service-framework/pkg/grpc"
+	"github.com/NpoolPlatform/notif-middleware/pkg/servicename"
 
 	"github.com/NpoolPlatform/libent-cruder/pkg/cruder"
 	npool "github.com/NpoolPlatform/message/npool/notif/mw/v1/contact"
-
-	constant "github.com/NpoolPlatform/notif-middleware/pkg/message/const"
 )
 
-var timeout = 10 * time.Second
-
-type handler func(context.Context, npool.MiddlewareClient) (cruder.Any, error)
-
-func withCRUD(ctx context.Context, handler handler) (cruder.Any, error) {
-	_ctx, cancel := context.WithTimeout(ctx, timeout)
+func do(ctx context.Context, fn func(_ctx context.Context, cli npool.MiddlewareClient) (cruder.Any, error)) (cruder.Any, error) {
+	_ctx, cancel := context.WithTimeout(ctx, 10*time.Second) //nolint
 	defer cancel()
 
-	conn, err := grpc2.GetGRPCConn(constant.ServiceName, grpc2.GRPCTAG)
+	conn, err := grpc2.GetGRPCConn(servicename.ServiceDomain, grpc2.GRPCTAG)
 	if err != nil {
-		return nil, fmt.Errorf("fail get contact connection: %v", err)
+		return nil, err
 	}
-
 	defer conn.Close()
 
 	cli := npool.NewMiddlewareClient(conn)
 
-	return handler(_ctx, cli)
+	return fn(_ctx, cli)
 }
 
 func GenerateContact(ctx context.Context, in *npool.GenerateContactRequest) (*npool.TextInfo, error) {
-	info, err := withCRUD(ctx, func(_ctx context.Context, cli npool.MiddlewareClient) (cruder.Any, error) {
+	info, err := do(ctx, func(_ctx context.Context, cli npool.MiddlewareClient) (cruder.Any, error) {
 		resp, err := cli.GenerateContact(ctx, in)
 		if err != nil {
 			return nil, fmt.Errorf("fail generate contact: %v", err)
@@ -45,4 +39,90 @@ func GenerateContact(ctx context.Context, in *npool.GenerateContactRequest) (*np
 		return nil, fmt.Errorf("fail generate contact: %v", err)
 	}
 	return info.(*npool.TextInfo), nil
+}
+
+func CreateContact(ctx context.Context, in *npool.ContactReq) (*npool.Contact, error) {
+	info, err := do(ctx, func(_ctx context.Context, cli npool.MiddlewareClient) (cruder.Any, error) {
+		resp, err := cli.CreateContact(ctx, &npool.CreateContactRequest{
+			Info: in,
+		})
+		if err != nil {
+			return nil, err
+		}
+		return resp.Info, nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	return info.(*npool.Contact), nil
+}
+
+func DeleteContact(ctx context.Context, id string) (*npool.Contact, error) {
+	info, err := do(ctx, func(_ctx context.Context, cli npool.MiddlewareClient) (cruder.Any, error) {
+		resp, err := cli.DeleteContact(ctx, &npool.DeleteContactRequest{
+			Info: &npool.ContactReq{
+				ID: &id,
+			},
+		})
+		if err != nil {
+			return nil, err
+		}
+		return resp.Info, nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	return info.(*npool.Contact), nil
+}
+
+func UpdateContact(ctx context.Context, in *npool.ContactReq) (*npool.Contact, error) {
+	info, err := do(ctx, func(_ctx context.Context, cli npool.MiddlewareClient) (cruder.Any, error) {
+		resp, err := cli.UpdateContact(ctx, &npool.UpdateContactRequest{
+			Info: in,
+		})
+		if err != nil {
+			return nil, err
+		}
+		return resp.Info, nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	return info.(*npool.Contact), nil
+}
+
+func GetContacts(ctx context.Context, conds *npool.Conds, offset, limit int32) ([]*npool.Contact, uint32, error) {
+	var total uint32
+	infos, err := do(ctx, func(_ctx context.Context, cli npool.MiddlewareClient) (cruder.Any, error) {
+		resp, err := cli.GetContacts(ctx, &npool.GetContactsRequest{
+			Conds:  conds,
+			Limit:  limit,
+			Offset: offset,
+		})
+		if err != nil {
+			return nil, fmt.Errorf("fail get announcements: %v", err)
+		}
+		total = resp.GetTotal()
+		return resp.GetInfos(), nil
+	})
+	if err != nil {
+		return nil, 0, fmt.Errorf("fail get announcements: %v", err)
+	}
+	return infos.([]*npool.Contact), total, nil
+}
+
+func GetContact(ctx context.Context, id string) (*npool.Contact, error) {
+	info, err := do(ctx, func(_ctx context.Context, cli npool.MiddlewareClient) (cruder.Any, error) {
+		resp, err := cli.GetContact(ctx, &npool.GetContactRequest{
+			ID: id,
+		})
+		if err != nil {
+			return nil, err
+		}
+		return resp.Info, nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	return info.(*npool.Contact), nil
 }

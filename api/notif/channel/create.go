e10df9b750f7122040378e3c0cd75547daa4d3f9
daqiangli <dqli161@163.com>
2023-06-15 12:15:57 +0800

    add create & delete & exist

---
+++ api/notif/channel/create.go
@@ -0,0 +1,39 @@
+package channel
+
+import (
+	"context"
+
+	"github.com/NpoolPlatform/go-service-framework/pkg/logger"
+	npool "github.com/NpoolPlatform/message/npool/notif/mw/v1/notif/channel"
+	channel1 "github.com/NpoolPlatform/notif-middleware/pkg/mw/notif/channel"
+
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+func (s *Server) CreateChannel(ctx context.Context, in *npool.CreateChannelRequest) (*npool.CreateChannelResponse, error) {
+	req := in.GetInfo()
+	handler, err := channel1.NewHandler(
+		ctx,
+		channel1.WithAppID(req.AppID),
+		channel1.WithEventType(req.EventType),
+		channel1.WithChannel(req.Channel),
+	)
+	if err != nil {
+		logger.Sugar().Errorw(
+			"CreateChannel",
+			"Req", in,
+			"Error", err,
+		)
+		return &npool.CreateChannelResponse{}, status.Error(codes.InvalidArgument, err.Error())
+	}
+
+	info, err := handler.CreateChannel(ctx)
+	if err != nil {
+		return &npool.CreateChannelResponse{}, status.Error(codes.Aborted, err.Error())
+	}
+
+	return &npool.CreateChannelResponse{
+		Info: info,
+	}, nil
+}

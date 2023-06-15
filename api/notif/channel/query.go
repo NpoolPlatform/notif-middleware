f2ebd93fd23d1a0f6aca0cd03195092e8c26f822
daqiangli <dqli161@163.com>
2023-06-15 12:22:47 +0800

    update query

--- api/notif/channel/query.go
+++ api/notif/channel/query.go
@@ -4,12 +4,7 @@ package channel
 import (
 	"context"
 
-	mgrcli "github.com/NpoolPlatform/notif-manager/pkg/client/notif/channel"
-
-	constant "github.com/NpoolPlatform/notif-middleware/pkg/message/const"
-
-	"go.opentelemetry.io/otel"
-	scodes "go.opentelemetry.io/otel/codes"
+	channel1 "github.com/NpoolPlatform/notif-middleware/pkg/mw/notif/channel"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 
@@ -17,63 +12,34 @@ import (
 	npool "github.com/NpoolPlatform/message/npool/notif/mw/v1/notif/channel"
 )
 
-func (s *Server) GetChannelOnly(
-	ctx context.Context,
-	in *npool.GetChannelOnlyRequest,
-) (
-	*npool.GetChannelOnlyResponse,
-	error,
-) {
-	var err error
-
-	_, span := otel.Tracer(constant.ServiceName).Start(ctx, "GetChannels")
-	defer span.End()
-
-	defer func() {
-		if err != nil {
-			span.SetStatus(scodes.Error, err.Error())
-			span.RecordError(err)
-		}
-	}()
-
-	row, err := mgrcli.GetChannelOnly(ctx, in.GetConds())
+func (s *Server) GetChannels(ctx context.Context, in *npool.GetChannelsRequest) (*npool.GetChannelsResponse, error) {
+	handler, err := channel1.NewHandler(
+		ctx,
+		channel1.WithConds(in.GetConds()),
+		channel1.WithOffset(in.Offset),
+		channel1.WithLimit(in.Limit),
+	)
 	if err != nil {
-		logger.Sugar().Errorw("GetChannels", "error", err)
-		return &npool.GetChannelOnlyResponse{}, status.Error(codes.Internal, err.Error())
+		logger.Sugar().Errorw(
+			"GetChannels",
+			"In", in,
+			"Error", err,
+		)
+		return &npool.GetChannelsResponse{}, status.Error(codes.InvalidArgument, err.Error())
 	}
 
-	return &npool.GetChannelOnlyResponse{
-		Info: row,
-	}, nil
-}
-
-func (s *Server) GetChannels(
-	ctx context.Context,
-	in *npool.GetChannelsRequest,
-) (
-	*npool.GetChannelsResponse,
-	error,
-) {
-	var err error
-
-	_, span := otel.Tracer(constant.ServiceName).Start(ctx, "GetChannels")
-	defer span.End()
-
-	defer func() {
-		if err != nil {
-			span.SetStatus(scodes.Error, err.Error())
-			span.RecordError(err)
-		}
-	}()
-
-	rows, total, err := mgrcli.GetChannels(ctx, in.GetConds(), in.GetOffset(), in.GetLimit())
+	infos, total, err := handler.GetChannels(ctx)
 	if err != nil {
-		logger.Sugar().Errorw("GetChannels", "error", err)
+		logger.Sugar().Errorw(
+			"GetChannels",
+			"Req", in,
+			"Error", err,
+		)
 		return &npool.GetChannelsResponse{}, status.Error(codes.Internal, err.Error())
 	}
 
 	return &npool.GetChannelsResponse{
-		Infos: rows,
+		Infos: infos,
 		Total: total,
 	}, nil
 }

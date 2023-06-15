cd9ac9230af3a718459dc745066f75c837feb809
daqiangli <dqli161@163.com>
2023-06-15 15:00:36 +0800

    add handler

---
+++ pkg/mw/notif/tx/handler.go
@@ -0,0 +1,142 @@
+package tx
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/NpoolPlatform/libent-cruder/pkg/cruder"
+	basetypes "github.com/NpoolPlatform/message/npool/basetypes/v1"
+	npool "github.com/NpoolPlatform/message/npool/notif/mw/v1/notif/tx"
+	constant "github.com/NpoolPlatform/notif-middleware/pkg/const"
+	crud "github.com/NpoolPlatform/notif-middleware/pkg/crud/notif/tx"
+	"github.com/google/uuid"
+)
+
+type Handler struct {
+	ID         *uuid.UUID
+	TxID       *uuid.UUID
+	NotifState *basetypes.TxState
+	TxType     *basetypes.TxType
+	Conds      *crud.Conds
+	Offset     int32
+	Limit      int32
+}
+
+func NewHandler(ctx context.Context, options ...func(context.Context, *Handler) error) (*Handler, error) {
+	handler := &Handler{}
+	for _, opt := range options {
+		if err := opt(ctx, handler); err != nil {
+			return nil, err
+		}
+	}
+	return handler, nil
+}
+
+func WithID(id *string) func(context.Context, *Handler) error {
+	return func(ctx context.Context, h *Handler) error {
+		_id, err := uuid.Parse(*id)
+		if err != nil {
+			return err
+		}
+		h.ID = &_id
+		return nil
+	}
+}
+
+func WithTxID(txID *string) func(context.Context, *Handler) error {
+	return func(ctx context.Context, h *Handler) error {
+		txID, err := uuid.Parse(*txID)
+		if err != nil {
+			return err
+		}
+		h.TxID = &txID
+		return nil
+	}
+}
+
+func WithTxType(tx *basetypes.TxType) func(context.Context, *Handler) error {
+	return func(ctx context.Context, h *Handler) error {
+		switch *tx {
+		case basetypes.TxType_TxWithdraw:
+		default:
+			return fmt.Errorf("tx %v invalid", *tx)
+		}
+		h.TxType = tx
+		return nil
+	}
+}
+
+func WithNotifState(state *basetypes.TxState) func(context.Context, *Handler) error {
+	return func(ctx context.Context, h *Handler) error {
+		if state == nil {
+			return nil
+		}
+		switch *state {
+		case basetypes.TxState_TxStateSuccessful:
+		case basetypes.TxState_TxStateWait:
+		case basetypes.TxState_TxStateCreated:
+		default:
+			return fmt.Errorf("tx state is invalid")
+		}
+
+		h.NotifState = state
+		return nil
+	}
+}
+
+func WithOffset(offset int32) func(context.Context, *Handler) error {
+	return func(ctx context.Context, h *Handler) error {
+		h.Offset = offset
+		return nil
+	}
+}
+
+func WithLimit(limit int32) func(context.Context, *Handler) error {
+	return func(ctx context.Context, h *Handler) error {
+		if limit == 0 {
+			limit = constant.DefaultRowLimit
+		}
+		h.Limit = limit
+		return nil
+	}
+}
+
+func WithConds(conds *npool.Conds) func(context.Context, *Handler) error {
+	return func(ctx context.Context, h *Handler) error {
+		h.Conds = &crud.Conds{}
+		if conds == nil {
+			return nil
+		}
+		if conds.ID != nil {
+			id, err := uuid.Parse(conds.GetID().GetValue())
+			if err != nil {
+				return err
+			}
+			h.Conds.ID = &cruder.Cond{
+				Op:  conds.GetID().GetOp(), Val: id,
+			}
+		}
+		if conds.TxID != nil {
+			appID, err := uuid.Parse(conds.GetTxID().GetValue())
+			if err != nil {
+				return err
+			}
+			h.Conds.TxID = &cruder.Cond{
+				Op:  conds.GetTxID().GetOp(), Val: appID,
+			}
+		}
+		if conds.TxType != nil {
+			_type := conds.GetTxType().GetValue()
+			h.Conds.TxType = &cruder.Cond{
+				Op:  conds.GetTxType().GetOp(), Val: basetypes.TxType(_type),
+			}
+		}
+		if conds.NotifState != nil {
+			_state := conds.GetNotifState().GetValue()
+			h.Conds.NotifState = &cruder.Cond{
+				Op:  conds.GetNotifState().GetOp(), Val: basetypes.TxState(_state),
+			}
+		}
+		return nil
+	}
+}

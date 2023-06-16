0e2e345ea7f892c614713b119b0a524d87961528
daqiangli <dqli161@163.com>
2023-06-16 10:48:25 +0800

    add create amt users

--- api/announcement/user/create.go
+++ api/announcement/user/create.go
@@ -6,14 +6,14 @@ import (
 	"github.com/NpoolPlatform/go-service-framework/pkg/logger"
 	npool "github.com/NpoolPlatform/message/npool/notif/mw/v1/announcement/user"
 	"github.com/NpoolPlatform/notif-middleware/pkg/mw/announcement/handler"
-	announcement1 "github.com/NpoolPlatform/notif-middleware/pkg/mw/announcement/user"
+	amtuser1 "github.com/NpoolPlatform/notif-middleware/pkg/mw/announcement/user"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
 
 func (s *Server) CreateAnnouncementUser(ctx context.Context, in *npool.CreateAnnouncementUserRequest) (*npool.CreateAnnouncementUserResponse, error) {
 	req := in.GetInfo()
-	handler, err := announcement1.NewHandler(
+	handler, err := amtuser1.NewHandler(
 		ctx,
 		handler.WithAppID(req.AppID),
 		handler.WithUserID(req.AppID, req.UserID),
@@ -30,10 +30,44 @@ func (s *Server) CreateAnnouncementUser(ctx context.Context, in *npool.CreateAnn
 
 	info, err := handler.CreateAnnouncementUser(ctx)
 	if err != nil {
+		logger.Sugar().Errorw(
+			"CreateAnnouncementUser",
+			"Req", in,
+			"Error", err,
+		)
 		return &npool.CreateAnnouncementUserResponse{}, status.Error(codes.Aborted, err.Error())
 	}
 
 	return &npool.CreateAnnouncementUserResponse{
 		Info: info,
+	}, nil
+}
+
+func (s *Server) CreateAnnouncementUsers(ctx context.Context, in *npool.CreateAnnouncementUsersRequest) (*npool.CreateAnnouncementUsersResponse, error) {
+	handler, err := amtuser1.NewHandler(
+		ctx,
+		amtuser1.WithReqs(in.GetInfos()),
+	)
+	if err != nil {
+		logger.Sugar().Errorw(
+			"CreateAnnouncementUsers",
+			"Req", in,
+			"Error", err,
+		)
+		return &npool.CreateAnnouncementUsersResponse{}, status.Error(codes.InvalidArgument, err.Error())
+	}
+
+	info, err := handler.CreateAnnouncementUsers(ctx)
+	if err != nil {
+		logger.Sugar().Errorw(
+			"CreateAnnouncementUsers",
+			"Req", in,
+			"Error", err,
+		)
+		return &npool.CreateAnnouncementUsersResponse{}, status.Error(codes.Aborted, err.Error())
+	}
+
+	return &npool.CreateAnnouncementUsersResponse{
+		Infos: info,
 	}, nil
 }

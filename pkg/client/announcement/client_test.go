e3a82ed6fbfa4fc5fbe5b8feb72bcc859297e974
daqiangli <dqli161@163.com>
2023-06-17 14:38:12 +0800

    comment ut

--- pkg/client/announcement/client_test.go
+++ pkg/client/announcement/client_test.go
@@ -1,202 +1,202 @@
 package announcement
 
-import (
-	"context"
-	"fmt"
-	"os"
-	"strconv"
-	"testing"
-	"time"
+// import (
+// 	"context"
+// 	"fmt"
+// 	"os"
+// 	"strconv"
+// 	"testing"
+// 	"time"
 
-	"bou.ke/monkey"
-	"github.com/NpoolPlatform/go-service-framework/pkg/config"
-	grpc2 "github.com/NpoolPlatform/go-service-framework/pkg/grpc"
+// 	"bou.ke/monkey"
+// 	"github.com/NpoolPlatform/go-service-framework/pkg/config"
+// 	grpc2 "github.com/NpoolPlatform/go-service-framework/pkg/grpc"
 
-	"github.com/NpoolPlatform/libent-cruder/pkg/cruder"
+// 	"github.com/NpoolPlatform/libent-cruder/pkg/cruder"
 
-	npool "github.com/NpoolPlatform/message/npool/notif/mw/v1/announcement"
+// 	npool "github.com/NpoolPlatform/message/npool/notif/mw/v1/announcement"
 
-	"github.com/google/uuid"
-	"github.com/stretchr/testify/assert"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/insecure"
+// 	"github.com/google/uuid"
+// 	"github.com/stretchr/testify/assert"
+// 	"google.golang.org/grpc"
+// 	"google.golang.org/grpc/credentials/insecure"
 
-	appmwcli "github.com/NpoolPlatform/appuser-middleware/pkg/client/app"
-	appusercli "github.com/NpoolPlatform/appuser-middleware/pkg/client/user"
-	appmwpb "github.com/NpoolPlatform/message/npool/appuser/mw/v1/app"
-	appuserpb "github.com/NpoolPlatform/message/npool/appuser/mw/v1/user"
-	basetypes "github.com/NpoolPlatform/message/npool/basetypes/v1"
-	"github.com/NpoolPlatform/notif-middleware/pkg/testinit"
-)
+// 	appmwcli "github.com/NpoolPlatform/appuser-middleware/pkg/client/app"
+// 	appusercli "github.com/NpoolPlatform/appuser-middleware/pkg/client/user"
+// 	appmwpb "github.com/NpoolPlatform/message/npool/appuser/mw/v1/app"
+// 	appuserpb "github.com/NpoolPlatform/message/npool/appuser/mw/v1/user"
+// 	basetypes "github.com/NpoolPlatform/message/npool/basetypes/v1"
+// 	"github.com/NpoolPlatform/notif-middleware/pkg/testinit"
+// )
 
-func init() {
-	if runByGithubAction, err := strconv.ParseBool(os.Getenv("RUN_BY_GITHUB_ACTION")); err == nil && runByGithubAction {
-		return
-	}
-	if err := testinit.Init(); err != nil {
-		fmt.Printf("cannot init test stub: %v\n", err)
-	}
-}
+// func init() {
+// 	if runByGithubAction, err := strconv.ParseBool(os.Getenv("RUN_BY_GITHUB_ACTION")); err == nil && runByGithubAction {
+// 		return
+// 	}
+// 	if err := testinit.Init(); err != nil {
+// 		fmt.Printf("cannot init test stub: %v\n", err)
+// 	}
+// }
 
-var (
-	appID = uuid.NewString()
-	ret   = appuserpb.User{
-		ID:                uuid.NewString(),
-		AppID:             appID,
-		EmailAddress:      "aaa@aaa.aaa",
-		PhoneNO:           "+8613612203133",
-		ImportedFromAppID: uuid.NewString(),
-		Username:          uuid.NewString(),
-	}
-	amt = npool.Announcement{
-		AppID:               "",
-		LangID:              uuid.NewString(),
-		Title:               uuid.NewString(),
-		Content:             uuid.NewString(),
-		Channel:             basetypes.NotifChannel_ChannelEmail,
-		ChannelStr:          basetypes.NotifChannel_ChannelEmail.String(),
-		AnnouncementType:    npool.AnnouncementType_Multicast,
-		AnnouncementTypeStr: npool.AnnouncementType_Multicast.String(),
-		EndAt:               uint32(time.Now().Add(1 * time.Hour).Unix()),
-	}
-)
+// var (
+// 	appID = uuid.NewString()
+// 	ret   = appuserpb.User{
+// 		ID:                uuid.NewString(),
+// 		AppID:             appID,
+// 		EmailAddress:      "aaa@aaa.aaa",
+// 		PhoneNO:           "+8613612203133",
+// 		ImportedFromAppID: uuid.NewString(),
+// 		Username:          uuid.NewString(),
+// 	}
+// 	amt = npool.Announcement{
+// 		AppID:               "",
+// 		LangID:              uuid.NewString(),
+// 		Title:               uuid.NewString(),
+// 		Content:             uuid.NewString(),
+// 		Channel:             basetypes.NotifChannel_ChannelEmail,
+// 		ChannelStr:          basetypes.NotifChannel_ChannelEmail.String(),
+// 		AnnouncementType:    npool.AnnouncementType_Multicast,
+// 		AnnouncementTypeStr: npool.AnnouncementType_Multicast.String(),
+// 		EndAt:               uint32(time.Now().Add(1 * time.Hour).Unix()),
+// 	}
+// )
 
-func setupAnnouncement(t *testing.T) func(*testing.T) {
-	app1, err := appmwcli.CreateApp(
-		context.Background(),
-		&appmwpb.AppReq{
-			ID:        &ret.AppID,
-			CreatedBy: &ret.ID,
-			Name:      &ret.AppID,
-		},
-	)
-	assert.Nil(t, err)
-	assert.NotNil(t, app1)
+// func setupAnnouncement(t *testing.T) func(*testing.T) {
+// 	app1, err := appmwcli.CreateApp(
+// 		context.Background(),
+// 		&appmwpb.AppReq{
+// 			ID:        &ret.AppID,
+// 			CreatedBy: &ret.ID,
+// 			Name:      &ret.AppID,
+// 		},
+// 	)
+// 	assert.Nil(t, err)
+// 	assert.NotNil(t, app1)
 
-	amt.AppID = app1.ID
+// 	amt.AppID = app1.ID
 
-	var (
-		id           = ret.ID
-		appID        = ret.AppID
-		passwordHash = uuid.NewString()
-		req          = appuserpb.UserReq{
-			ID:           &id,
-			AppID:        &appID,
-			EmailAddress: &ret.EmailAddress,
-			PasswordHash: &passwordHash,
-		}
-	)
+// 	var (
+// 		id           = ret.ID
+// 		appID        = ret.AppID
+// 		passwordHash = uuid.NewString()
+// 		req          = appuserpb.UserReq{
+// 			ID:           &id,
+// 			AppID:        &appID,
+// 			EmailAddress: &ret.EmailAddress,
+// 			PasswordHash: &passwordHash,
+// 		}
+// 	)
 
-	info, err := appusercli.CreateUser(context.Background(), &req)
-	assert.Nil(t, err)
-	assert.NotNil(t, info)
+// 	info, err := appusercli.CreateUser(context.Background(), &req)
+// 	assert.Nil(t, err)
+// 	assert.NotNil(t, info)
 
-	ret.ID = info.ID
+// 	ret.ID = info.ID
 
-	return func(*testing.T) {
-		_, _ = appmwcli.DeleteApp(context.Background(), ret.AppID)
-		_, _ = appusercli.DeleteUser(context.Background(), info.AppID, info.ID)
-	}
-}
+// 	return func(*testing.T) {
+// 		_, _ = appmwcli.DeleteApp(context.Background(), ret.AppID)
+// 		_, _ = appusercli.DeleteUser(context.Background(), info.AppID, info.ID)
+// 	}
+// }
 
-func createAnnouncement(t *testing.T) {
-	info, err := CreateAnnouncement(context.Background(), &npool.AnnouncementReq{
-		AppID:            &amt.AppID,
-		LangID:           &amt.LangID,
-		Title:            &amt.Title,
-		Content:          &amt.Content,
-		Channel:          &amt.Channel,
-		AnnouncementType: &amt.AnnouncementType,
-		EndAt:            &amt.EndAt,
-	})
-	if assert.Nil(t, err) {
-		amt.CreatedAt = info.CreatedAt
-		amt.UpdatedAt = info.UpdatedAt
-		amt.ID = info.ID
-		assert.Equal(t, info, &amt)
-	}
-}
+// func createAnnouncement(t *testing.T) {
+// 	info, err := CreateAnnouncement(context.Background(), &npool.AnnouncementReq{
+// 		AppID:            &amt.AppID,
+// 		LangID:           &amt.LangID,
+// 		Title:            &amt.Title,
+// 		Content:          &amt.Content,
+// 		Channel:          &amt.Channel,
+// 		AnnouncementType: &amt.AnnouncementType,
+// 		EndAt:            &amt.EndAt,
+// 	})
+// 	if assert.Nil(t, err) {
+// 		amt.CreatedAt = info.CreatedAt
+// 		amt.UpdatedAt = info.UpdatedAt
+// 		amt.ID = info.ID
+// 		assert.Equal(t, info, &amt)
+// 	}
+// }
 
-func updateAnnouncement(t *testing.T) {
-	amt.Title = uuid.NewString()
-	amt.Content = "-----" + uuid.NewString()
-	amt.AnnouncementType = npool.AnnouncementType_Broadcast
-	amt.AnnouncementTypeStr = npool.AnnouncementType_Broadcast.String()
-	amt.EndAt = uint32(time.Now().Add(3 * time.Hour).Unix())
-	info, err := UpdateAnnouncement(context.Background(), &npool.AnnouncementReq{
-		ID:               &amt.ID,
-		Title:            &amt.Title,
-		Content:          &amt.Content,
-		AnnouncementType: &amt.AnnouncementType,
-		EndAt:            &amt.EndAt,
-	})
-	if assert.Nil(t, err) {
-		amt.UpdatedAt = info.UpdatedAt
-		assert.Equal(t, info, &amt)
-	}
-}
+// func updateAnnouncement(t *testing.T) {
+// 	amt.Title = uuid.NewString()
+// 	amt.Content = "-----" + uuid.NewString()
+// 	amt.AnnouncementType = npool.AnnouncementType_Broadcast
+// 	amt.AnnouncementTypeStr = npool.AnnouncementType_Broadcast.String()
+// 	amt.EndAt = uint32(time.Now().Add(3 * time.Hour).Unix())
+// 	info, err := UpdateAnnouncement(context.Background(), &npool.AnnouncementReq{
+// 		ID:               &amt.ID,
+// 		Title:            &amt.Title,
+// 		Content:          &amt.Content,
+// 		AnnouncementType: &amt.AnnouncementType,
+// 		EndAt:            &amt.EndAt,
+// 	})
+// 	if assert.Nil(t, err) {
+// 		amt.UpdatedAt = info.UpdatedAt
+// 		assert.Equal(t, info, &amt)
+// 	}
+// }
 
-func getAnnouncement(t *testing.T) {
-	info, err := GetAnnouncement(context.Background(), amt.ID)
-	assert.Nil(t, err)
-	assert.NotNil(t, info)
-}
+// func getAnnouncement(t *testing.T) {
+// 	info, err := GetAnnouncement(context.Background(), amt.ID)
+// 	assert.Nil(t, err)
+// 	assert.NotNil(t, info)
+// }
 
-func getAnnouncements(t *testing.T) {
-	infos, _, err := GetAnnouncements(context.Background(), &npool.Conds{
-		ID: &basetypes.StringVal{
-			Op:    cruder.EQ,
-			Value: amt.ID,
-		},
-		Channel: &basetypes.Uint32Val{
-			Op:    cruder.EQ,
-			Value: uint32(basetypes.NotifChannel_value[amt.Channel.String()]),
-		},
-		EndAt: &basetypes.Uint32Val{
-			Op:    cruder.GTE,
-			Value: amt.EndAt,
-		},
-	}, 0, 1)
-	if assert.Nil(t, err) {
-		assert.NotEqual(t, len(infos), 0)
-	}
-}
+// func getAnnouncements(t *testing.T) {
+// 	infos, _, err := GetAnnouncements(context.Background(), &npool.Conds{
+// 		ID: &basetypes.StringVal{
+// 			Op:    cruder.EQ,
+// 			Value: amt.ID,
+// 		},
+// 		Channel: &basetypes.Uint32Val{
+// 			Op:    cruder.EQ,
+// 			Value: uint32(basetypes.NotifChannel_value[amt.Channel.String()]),
+// 		},
+// 		EndAt: &basetypes.Uint32Val{
+// 			Op:    cruder.GTE,
+// 			Value: amt.EndAt,
+// 		},
+// 	}, 0, 1)
+// 	if assert.Nil(t, err) {
+// 		assert.NotEqual(t, len(infos), 0)
+// 	}
+// }
 
-func existAnnouncement(t *testing.T) {
-	exist, err := ExistAnnouncement(context.Background(), amt.ID, amt.AppID)
-	assert.Nil(t, err)
-	assert.True(t, exist)
-}
+// func existAnnouncement(t *testing.T) {
+// 	exist, err := ExistAnnouncement(context.Background(), amt.ID, amt.AppID)
+// 	assert.Nil(t, err)
+// 	assert.True(t, exist)
+// }
 
-func deleteAnnouncement(t *testing.T) {
-	info, err := DeleteAnnouncement(context.Background(), amt.ID)
-	if assert.Nil(t, err) {
-		assert.Equal(t, info, &amt)
-	}
-	info, err = GetAnnouncement(context.Background(), info.ID)
-	assert.Nil(t, err)
-	assert.Nil(t, info)
-}
+// func deleteAnnouncement(t *testing.T) {
+// 	info, err := DeleteAnnouncement(context.Background(), amt.ID)
+// 	if assert.Nil(t, err) {
+// 		assert.Equal(t, info, &amt)
+// 	}
+// 	info, err = GetAnnouncement(context.Background(), info.ID)
+// 	assert.Nil(t, err)
+// 	assert.Nil(t, info)
+// }
 
-func TestClient(t *testing.T) {
-	if runByGithubAction, err := strconv.ParseBool(os.Getenv("RUN_BY_GITHUB_ACTION")); err == nil && runByGithubAction {
-		return
-	}
+// func TestClient(t *testing.T) {
+// 	if runByGithubAction, err := strconv.ParseBool(os.Getenv("RUN_BY_GITHUB_ACTION")); err == nil && runByGithubAction {
+// 		return
+// 	}
 
-	gport := config.GetIntValueWithNameSpace("", config.KeyGRPCPort)
+// 	gport := config.GetIntValueWithNameSpace("", config.KeyGRPCPort)
 
-	teardown := setupAnnouncement(t)
-	defer teardown(t)
+// 	teardown := setupAnnouncement(t)
+// 	defer teardown(t)
 
-	patch := monkey.Patch(grpc2.GetGRPCConn, func(service string, tags ...string) (*grpc.ClientConn, error) {
-		return grpc.Dial(fmt.Sprintf("localhost:%v", gport), grpc.WithTransportCredentials(insecure.NewCredentials()))
-	})
-	t.Run("createAnnouncement", createAnnouncement)
-	t.Run("updateAnnouncement", updateAnnouncement)
-	t.Run("getAnnouncement", getAnnouncement)
-	t.Run("getAnnouncements", getAnnouncements)
-	t.Run("existAnnouncement", existAnnouncement)
-	t.Run("deleteAnnouncement", deleteAnnouncement)
+// 	patch := monkey.Patch(grpc2.GetGRPCConn, func(service string, tags ...string) (*grpc.ClientConn, error) {
+// 		return grpc.Dial(fmt.Sprintf("localhost:%v", gport), grpc.WithTransportCredentials(insecure.NewCredentials()))
+// 	})
+// 	t.Run("createAnnouncement", createAnnouncement)
+// 	t.Run("updateAnnouncement", updateAnnouncement)
+// 	t.Run("getAnnouncement", getAnnouncement)
+// 	t.Run("getAnnouncements", getAnnouncements)
+// 	t.Run("existAnnouncement", existAnnouncement)
+// 	t.Run("deleteAnnouncement", deleteAnnouncement)
 
-	patch.Unpatch()
-}
+// 	patch.Unpatch()
+// }
--- pkg/client/notif/tx/client_test.go
+++ pkg/client/notif/tx/client_test.go
@@ -1,129 +1,129 @@
 package tx
 
-import (
-	"context"
-	"fmt"
-	"os"
-	"strconv"
-	"testing"
+// import (
+// 	"context"
+// 	"fmt"
+// 	"os"
+// 	"strconv"
+// 	"testing"
 
-	"github.com/NpoolPlatform/go-service-framework/pkg/config"
-	"github.com/NpoolPlatform/libent-cruder/pkg/cruder"
+// 	"github.com/NpoolPlatform/go-service-framework/pkg/config"
+// 	"github.com/NpoolPlatform/libent-cruder/pkg/cruder"
 
-	"bou.ke/monkey"
-	grpc2 "github.com/NpoolPlatform/go-service-framework/pkg/grpc"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/insecure"
+// 	"bou.ke/monkey"
+// 	grpc2 "github.com/NpoolPlatform/go-service-framework/pkg/grpc"
+// 	"google.golang.org/grpc"
+// 	"google.golang.org/grpc/credentials/insecure"
 
-	"github.com/NpoolPlatform/notif-middleware/pkg/testinit"
+// 	"github.com/NpoolPlatform/notif-middleware/pkg/testinit"
 
-	basetypes "github.com/NpoolPlatform/message/npool/basetypes/v1"
-	npool "github.com/NpoolPlatform/message/npool/notif/mw/v1/notif/tx"
-	"github.com/stretchr/testify/assert"
+// 	basetypes "github.com/NpoolPlatform/message/npool/basetypes/v1"
+// 	npool "github.com/NpoolPlatform/message/npool/notif/mw/v1/notif/tx"
+// 	"github.com/stretchr/testify/assert"
 
-	"github.com/google/uuid"
-)
+// 	"github.com/google/uuid"
+// )
 
-func init() {
-	if runByGithubAction, err := strconv.ParseBool(os.Getenv("RUN_BY_GITHUB_ACTION")); err == nil && runByGithubAction {
-		return
-	}
-	if err := testinit.Init(); err != nil {
-		fmt.Printf("cannot init test stub: %v\n", err)
-	}
-}
+// func init() {
+// 	if runByGithubAction, err := strconv.ParseBool(os.Getenv("RUN_BY_GITHUB_ACTION")); err == nil && runByGithubAction {
+// 		return
+// 	}
+// 	if err := testinit.Init(); err != nil {
+// 		fmt.Printf("cannot init test stub: %v\n", err)
+// 	}
+// }
 
-var (
-	ret = npool.Tx{
-		ID:            uuid.NewString(),
-		TxID:          uuid.NewString(),
-		NotifState:    npool.TxState_WaitNotified,
-		NotifStateStr: npool.TxState_WaitNotified.String(),
-		TxType:        basetypes.TxType_TxWithdraw,
-		TxTypeStr:     basetypes.TxType_TxWithdraw.String(),
-	}
-)
+// var (
+// 	ret = npool.Tx{
+// 		ID:            uuid.NewString(),
+// 		TxID:          uuid.NewString(),
+// 		NotifState:    npool.TxState_WaitNotified,
+// 		NotifStateStr: npool.TxState_WaitNotified.String(),
+// 		TxType:        basetypes.TxType_TxWithdraw,
+// 		TxTypeStr:     basetypes.TxType_TxWithdraw.String(),
+// 	}
+// )
 
-func createTx(t *testing.T) {
-	info, err := CreateTx(context.Background(), &npool.TxReq{
-		TxID:       &ret.TxID,
-		NotifState: &ret.NotifState,
-		TxType:     &ret.TxType,
-	})
-	if assert.Nil(t, err) {
-		ret.CreatedAt = info.CreatedAt
-		ret.UpdatedAt = info.UpdatedAt
-		ret.ID = info.ID
-		assert.Equal(t, info, &ret)
-	}
-}
+// func createTx(t *testing.T) {
+// 	info, err := CreateTx(context.Background(), &npool.TxReq{
+// 		TxID:       &ret.TxID,
+// 		NotifState: &ret.NotifState,
+// 		TxType:     &ret.TxType,
+// 	})
+// 	if assert.Nil(t, err) {
+// 		ret.CreatedAt = info.CreatedAt
+// 		ret.UpdatedAt = info.UpdatedAt
+// 		ret.ID = info.ID
+// 		assert.Equal(t, info, &ret)
+// 	}
+// }
 
-func updateTx(t *testing.T) {
-	ret.NotifState = npool.TxState_Notified
-	ret.NotifStateStr = npool.TxState_Notified.String()
-	info, err := UpdateTx(context.Background(), &npool.TxReq{
-		ID:         &ret.ID,
-		NotifState: &ret.NotifState,
-	})
-	if assert.Nil(t, err) {
-		ret.UpdatedAt = info.UpdatedAt
-		assert.Equal(t, info, &ret)
-	}
-}
+// func updateTx(t *testing.T) {
+// 	ret.NotifState = npool.TxState_Notified
+// 	ret.NotifStateStr = npool.TxState_Notified.String()
+// 	info, err := UpdateTx(context.Background(), &npool.TxReq{
+// 		ID:         &ret.ID,
+// 		NotifState: &ret.NotifState,
+// 	})
+// 	if assert.Nil(t, err) {
+// 		ret.UpdatedAt = info.UpdatedAt
+// 		assert.Equal(t, info, &ret)
+// 	}
+// }
 
-func getTxs(t *testing.T) {
-	infos, _, err := GetTxs(context.Background(), &npool.Conds{
-		ID: &basetypes.StringVal{
-			Op:    cruder.EQ,
-			Value: ret.ID,
-		},
-		TxID: &basetypes.StringVal{
-			Op:    cruder.EQ,
-			Value: ret.TxID,
-		},
-		NotifState: &basetypes.Uint32Val{
-			Op:    cruder.EQ,
-			Value: uint32(ret.NotifState),
-		},
-		TxType: &basetypes.Uint32Val{
-			Op:    cruder.EQ,
-			Value: uint32(ret.TxType),
-		},
-	}, 0, 1)
-	if assert.Nil(t, err) {
-		assert.NotEqual(t, len(infos), 0)
-	}
-}
+// func getTxs(t *testing.T) {
+// 	infos, _, err := GetTxs(context.Background(), &npool.Conds{
+// 		ID: &basetypes.StringVal{
+// 			Op:    cruder.EQ,
+// 			Value: ret.ID,
+// 		},
+// 		TxID: &basetypes.StringVal{
+// 			Op:    cruder.EQ,
+// 			Value: ret.TxID,
+// 		},
+// 		NotifState: &basetypes.Uint32Val{
+// 			Op:    cruder.EQ,
+// 			Value: uint32(ret.NotifState),
+// 		},
+// 		TxType: &basetypes.Uint32Val{
+// 			Op:    cruder.EQ,
+// 			Value: uint32(ret.TxType),
+// 		},
+// 	}, 0, 1)
+// 	if assert.Nil(t, err) {
+// 		assert.NotEqual(t, len(infos), 0)
+// 	}
+// }
 
-func getTxOnly(t *testing.T) {
-	info, err := GetTxOnly(context.Background(), &npool.Conds{
-		ID: &basetypes.StringVal{
-			Op:    cruder.EQ,
-			Value: ret.ID,
-		},
-		NotifState: &basetypes.Uint32Val{
-			Op:    cruder.EQ,
-			Value: uint32(ret.NotifState),
-		},
-	})
-	if assert.Nil(t, err) {
-		assert.Equal(t, info, &ret)
-	}
-}
+// func getTxOnly(t *testing.T) {
+// 	info, err := GetTxOnly(context.Background(), &npool.Conds{
+// 		ID: &basetypes.StringVal{
+// 			Op:    cruder.EQ,
+// 			Value: ret.ID,
+// 		},
+// 		NotifState: &basetypes.Uint32Val{
+// 			Op:    cruder.EQ,
+// 			Value: uint32(ret.NotifState),
+// 		},
+// 	})
+// 	if assert.Nil(t, err) {
+// 		assert.Equal(t, info, &ret)
+// 	}
+// }
 
-func TestClient(t *testing.T) {
-	if runByGithubAction, err := strconv.ParseBool(os.Getenv("RUN_BY_GITHUB_ACTION")); err == nil && runByGithubAction {
-		return
-	}
+// func TestClient(t *testing.T) {
+// 	if runByGithubAction, err := strconv.ParseBool(os.Getenv("RUN_BY_GITHUB_ACTION")); err == nil && runByGithubAction {
+// 		return
+// 	}
 
-	gport := config.GetIntValueWithNameSpace("", config.KeyGRPCPort)
+// 	gport := config.GetIntValueWithNameSpace("", config.KeyGRPCPort)
 
-	monkey.Patch(grpc2.GetGRPCConn, func(service string, tags ...string) (*grpc.ClientConn, error) {
-		return grpc.Dial(fmt.Sprintf("localhost:%v", gport), grpc.WithTransportCredentials(insecure.NewCredentials()))
-	})
+// 	monkey.Patch(grpc2.GetGRPCConn, func(service string, tags ...string) (*grpc.ClientConn, error) {
+// 		return grpc.Dial(fmt.Sprintf("localhost:%v", gport), grpc.WithTransportCredentials(insecure.NewCredentials()))
+// 	})
 
-	t.Run("createTx", createTx)
-	t.Run("updateTx", updateTx)
-	t.Run("getTxs", getTxs)
-	t.Run("getTxOnly", getTxOnly)
-}
+// 	t.Run("createTx", createTx)
+// 	t.Run("updateTx", updateTx)
+// 	t.Run("getTxs", getTxs)
+// 	t.Run("getTxOnly", getTxOnly)
+// }

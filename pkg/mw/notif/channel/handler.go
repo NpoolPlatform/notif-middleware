70222d6040685af082f303c01506fb83d23e4c2f
daqiangli <dqli161@163.com>
2023-07-25 12:21:36 +0800

    add used for when create channel

--- pkg/mw/notif/channel/handler.go
+++ pkg/mw/notif/channel/handler.go
@@ -89,6 +89,12 @@ func WithEventType(_type *basetypes.UsedFor) func(context.Context, *Handler) err
 		case basetypes.UsedFor_KYCRejected:
 		case basetypes.UsedFor_Announcement:
 		case basetypes.UsedFor_GoodBenefit1:
+		case basetypes.UsedFor_UpdateEmail:
+		case basetypes.UsedFor_UpdateMobile:
+		case basetypes.UsedFor_UpdatePassword:
+		case basetypes.UsedFor_UpdateGoogleAuth:
+		case basetypes.UsedFor_NewDeviceDetected:
+		case basetypes.UsedFor_OrderCompleted:
 		default:
 			return fmt.Errorf("EventType is invalid")
 		}

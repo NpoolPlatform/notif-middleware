ac789fb20c3fbc39fe9e2cf7d96e02820af809d6
Zhao KK <kikakkz@hotmail.com>
2023-09-06 14:05:17 +0000

    Add api

---
+++ pkg/mw/notif/goodbenefit/exist.go
@@ -0,0 +1,29 @@
+package goodbenefit
+
+import (
+	"context"
+
+	crud "github.com/NpoolPlatform/notif-middleware/pkg/crud/notif/goodbenefit"
+	"github.com/NpoolPlatform/notif-middleware/pkg/db"
+	"github.com/NpoolPlatform/notif-middleware/pkg/db/ent"
+)
+
+func (h *Handler) ExistGoodBenefitConds(ctx context.Context) (bool, error) {
+	exist := false
+	err := db.WithClient(ctx, func(_ctx context.Context, cli *ent.Client) error {
+		stm, err := crud.SetQueryConds(cli.GoodBenefit.Query(), h.Conds)
+		if err != nil {
+			return err
+		}
+		exist, err = stm.Exist(_ctx)
+		if err != nil {
+			return err
+		}
+		return nil
+	})
+	if err != nil {
+		return false, err
+	}
+
+	return exist, nil
+}
